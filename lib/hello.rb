8550b8562708ef4d6cc8cade6d87508de2b6532e
simone.rainieri <simone.rainieri@gmail.com>
2019-02-12 14:31:35 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -9,12 +9,15 @@ end
 
 
 def hello_t(names)
+  if block_given?
 i = 0 
 while i < names.length
 yield(names[i])
 i = i + 1 
 end
 names
+else 
+  puts "Hey! No block was given!"
 end
 # call your method here!
 
